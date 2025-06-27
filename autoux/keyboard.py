f0f5b7e67e3f28d537c732aefeb081b4bfba00ad
Aditya Shriwastava <adityashriwastava7@gmail.com>
2025-06-27 16:55:39 +0530

    Add keyboard and screen

---
+++ autoux/keyboard.py
@@ -0,0 +1,91 @@
+import time
+from threading import Thread
+
+from pynput.keyboard import Key, Controller
+
+class Keyboard:
+    def __init__(self, control_hz: float = 25.0):
+        self.control_hz = control_hz
+
+        self.controller = Controller()
+        self.control_thread = Thread(target=self.control_loop)
+
+        # Human-readable key map
+        self.KEY_MAP = {
+            'alt': Key.alt,
+            'alt_l': Key.alt_l,
+            'alt_r': Key.alt_r,
+            'backspace': Key.backspace,
+            'caps_lock': Key.caps_lock,
+            'cmd': Key.cmd,
+            'cmd_l': Key.cmd_l,
+            'cmd_r': Key.cmd_r,
+            'ctrl': Key.ctrl,
+            'ctrl_l': Key.ctrl_l,
+            'ctrl_r': Key.ctrl_r,
+            'delete': Key.delete,
+            'down': Key.down,
+            'end': Key.end,
+            'enter': Key.enter,
+            'esc': Key.esc,
+            'f1': Key.f1,
+            'f2': Key.f2,
+            'f3': Key.f3,
+            'f4': Key.f4,
+            'f5': Key.f5,
+            'f6': Key.f6,
+            'f7': Key.f7,
+            'f8': Key.f8,
+            'f9': Key.f9,
+            'f10': Key.f10,
+            'f11': Key.f11,
+            'f12': Key.f12,
+            'home': Key.home,
+            'left': Key.left,
+            'page_down': Key.page_down,
+            'page_up': Key.page_up,
+            'right': Key.right,
+            'shift': Key.shift,
+            'shift_l': Key.shift_l,
+            'shift_r': Key.shift_r,
+            'space': Key.space,
+            'tab': Key.tab,
+            'up': Key.up,
+            # Alphanumeric and printable ASCII keys
+            'a': 'a', 'b': 'b', 'c': 'c', 'd': 'd', 'e': 'e', 'f': 'f', 'g': 'g', 'h': 'h', 'i': 'i', 'j': 'j', 'k': 'k', 'l': 'l', 'm': 'm', 'n': 'n', 'o': 'o', 'p': 'p', 'q': 'q', 'r': 'r', 's': 's', 't': 't', 'u': 'u', 'v': 'v', 'w': 'w', 'x': 'x', 'y': 'y', 'z': 'z',
+            'A': 'A', 'B': 'B', 'C': 'C', 'D': 'D', 'E': 'E', 'F': 'F', 'G': 'G', 'H': 'H', 'I': 'I', 'J': 'J', 'K': 'K', 'L': 'L', 'M': 'M', 'N': 'N', 'O': 'O', 'P': 'P', 'Q': 'Q', 'R': 'R', 'S': 'S', 'T': 'T', 'U': 'U', 'V': 'V', 'W': 'W', 'X': 'X', 'Y': 'Y', 'Z': 'Z',
+            '0': '0', '1': '1', '2': '2', '3': '3', '4': '4', '5': '5', '6': '6', '7': '7', '8': '8', '9': '9',
+            '`': '`', '~': '~', '!': '!', '@': '@', '#': '#', '$': '$', '%': '%', '^': '^', '&': '&', '*': '*', '(': '(', ')': ')', '-': '-', '_': '_', '=': '=', '+': '+', '[': '[', ']': ']', '{': '{', '}': '}', '\\': '\\', '|': '|', ';': ';', ':': ':', '\'': '\'', '"': '"', ',': ',', '<': '<', '.': '.', '>': '>', '/': '/', '?': '?', ' ': ' '
+        }
+
+        self.buffer: list[tuple[str, str]] = []
+        self.done = False
+        self.control_thread.start()
+
+    def control_loop(self):
+        while not self.done:
+            if len(self.buffer) > 0:
+                action, key = self.buffer.pop(0)
+                if action == 'press':
+                    self.controller.press(self.KEY_MAP[key])
+                elif action == 'release':
+                    self.controller.release(self.KEY_MAP[key])
+            time.sleep(1 / self.control_hz)
+
+    def press_key(self, key: str):
+        """
+        Press a key on the keyboard.
+        key: The key to press
+        """
+        self.buffer.append(('press', key))
+
+    def release_key(self, key: str):
+        """
+        Release a key on the keyboard.
+        key: The key to release
+        """
+        self.buffer.append(('release', key))
+
+    def cleanup(self):
+        self.done = True
+        self.control_thread.join()
---
+++ autoux/screen.py
@@ -0,0 +1,45 @@
+import numpy as np
+from mss import mss
+from PIL import Image, ImageDraw
+
+
+class Screen:
+    def __init__(self):
+        self.sct = mss()
+        self.monitor = self.sct.monitors[1]
+
+    def capture(self, cursor_pos: tuple[int, int] = None):
+        """
+        Capture the current screen and return it as a NumPy array (H, W, 3 RGB, dtype=uint8), with the mouse cursor overlaid if cursor_pos is not None.
+        """
+
+        img = self.sct.grab(self.monitor)
+        pil_img = Image.frombytes(
+            'RGB',
+            img.size,
+            img.rgb
+        )
+
+        if cursor_pos is not None:
+            self.draw_cursor(pil_img, cursor_pos[0], cursor_pos[1])
+
+        return np.array(pil_img)
+
+    def draw_cursor(self, image: Image.Image, x: int, y: int):
+        """
+        Draw a circular cursor: black fill with thick white border at the given relative position on the provided PIL image.
+        """
+        circle_radius = 8
+        outline_width = 3
+        if 0 <= x < image.width and 0 <= y < image.height:
+            draw = ImageDraw.Draw(image)
+            # Draw white border (outer circle)
+            draw.ellipse([
+                (x - circle_radius - outline_width, y - circle_radius - outline_width),
+                (x + circle_radius + outline_width, y + circle_radius + outline_width)
+            ], fill="white")
+            # Draw black filled circle (inner)
+            draw.ellipse([
+                (x - circle_radius, y - circle_radius),
+                (x + circle_radius, y + circle_radius)
+            ], fill="black")
